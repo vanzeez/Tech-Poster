4d593cdb1f8aa9b3c84b6360a595b8d3761c40cb
Vanshika Gupta <124513445+vanzeez@users.noreply.github.com>
2023-09-08 18:41:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,45 +2,41 @@ function addHoverEffects() {
     const container = document.querySelector('.container');
     const poster = document.querySelector('.poster img');
     const heading = document.querySelector('.heading');
-    const eventDetails = document.querySelector('.event-details');
+
+    // Apply box shadow and scale on container hover
     container.addEventListener('mouseover', () => {
         container.style.boxShadow = '0 0 40px rgba(255, 0, 0, 0.8)';
     });
     container.addEventListener('mouseout', () => {
         container.style.boxShadow = '0 0 20px rgba(255, 0, 0, 0.5)';
     });
+
+    // Apply scale on poster hover
     poster.addEventListener('mouseover', () => {
         poster.style.transform = 'scale(1.1)';
     });
     poster.addEventListener('mouseout', () => {
         poster.style.transform = 'scale(1)';
     });
+
+    // Change heading color on hover
     heading.addEventListener('mouseover', () => {
         heading.style.color = 'red';
     });
     heading.addEventListener('mouseout', () => {
         heading.style.color = '#ffcc00';
     });
-    eventDetails.addEventListener('mouseover', () => {
-    });
-    eventDetails.addEventListener('mouseout', () => {
-    });
 }
+
+// Call the function to add hover effects
 addHoverEffects();
+
 particlesJS('particles-js', {
     particles: {
-        number: { value: 200, density: { enable: true, value_area: 800 } }, // Increased number of particles
-        color: { value: '#ffffff' },
-        shape: { type: 'circle', stroke: { width: 0, color: '#000000' }, polygon: { nb_sides: 5 } },
-        opacity: { value: 0.2, random: false, anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false } },
-        size: { value: 3, random: true, anim: { enable: false, speed: 40, size_min: 0.1, sync: false } },
-        line_linked: { enable: true, distance: 150, color: '#ffffff', opacity: 0.4, width: 1 },
-        move: { enable: true, speed: 6, direction: 'none', random: false, straight: false, out_mode: 'out', bounce: false, attract: { enable: false, rotateX: 600, rotateY: 1200 } }
+        // Your particle settings...
     },
     interactivity: {
-        detect_on: 'canvas',
-        events: { onhover: { enable: true, mode: 'repulse' }, onclick: { enable: true, mode: 'push' }, resize: true },
-        modes: { grab: { distance: 400, line_linked: { opacity: 1 } }, bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 }, repulse: { distance: 200, duration: 0.4 }, push: { particles_nb: 4 }, remove: { particles_nb: 2 } }
+        // Your interactivity settings...
     },
     retina_detect: true
 });
